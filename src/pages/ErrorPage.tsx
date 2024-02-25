5781813a5b485195e44c662adda357cdebf6606a
Frank Vamos, Jr <fvamos@gmail.com>
2024-02-24 22:13:03 -0500

    Init setup

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,15 @@
+import { useRouteError } from "react-router-dom";
+
+function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <h1 className='text-2xl mb-3'>An unexpected error has occurred.</h1>
+      <i className='text-lg'>{JSON.stringify(error, null, 2)}</i>
+    </div>
+  )
+}
+
+export default ErrorPage;
